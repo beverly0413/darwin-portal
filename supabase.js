c826b3f3aa32651435118690686fbd493d470ce3
beverly0413 <chou04131498917494@gmail.com>
2025-11-20 14:54:04 +0930

    update index.html with supabase.js
    Commit 2 files to main

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+export const supabase = createClient(
+  'https://lhmvgvvquxnljlncyvke.supabase.co',
+  'sb_publishable_UQaE3FubMzqslw-JC1cIA_vKhIbXjx'
+);
