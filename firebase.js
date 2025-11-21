f41952383cb0e8d9bf1163084c23ec3b4da75863
beverly0413 <chou04131498917494@gmail.com>
2025-11-21 21:03:53 +0930

    updated firebase 2

--- firebase.js
+++ firebase.js
@@ -2,22 +2,22 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// 你的 Firebase 配置
+// ❗必须与 Firebase 控制台完全一致的配置
 const firebaseConfig = {
-  apiKey: "AIzaSyBMQ8iHk-rZxEUBuf18uksrgfwzM8LZFY",
+  apiKey: "AIzaSyBMqBiGHK-rZxEUBUf18uksrcgwfZM8LZFY",
   authDomain: "darwin-portal.firebaseapp.com",
   projectId: "darwin-portal",
-  storageBucket: "darwin-portal.appspot.com",
+  storageBucket: "darwin-portal.firebasestorage.app",
   messagingSenderId: "178556543442",
-  appId: "1:178556543442:web:ab1cee82835d98b8c0680b",
-  measurementId: "G-R7LQ8MLYH69",
+  appId: "1:178556543442:web:abc1ee82835d98b8c0680b",
+  measurementId: "G-R7LQ8MLYH69"
 };
 
 // 初始化
 export const app = initializeApp(firebaseConfig);
 
-// Firestore（必需）
+// Firestore
 export const db = getFirestore(app);
 
-// Auth（后台登录必需）🔥🔥🔥
+// Auth（后台登录用）
 export const auth = getAuth(app);
