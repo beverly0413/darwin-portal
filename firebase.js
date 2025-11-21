c464e20b04f69734140cbb8fd63871aef002d54b
beverly0413 <chou04131498917494@gmail.com>
2025-11-21 20:57:39 +0930

    update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
+// 你的 Firebase 配置
 const firebaseConfig = {
   apiKey: "AIzaSyBMQ8iHk-rZxEUBuf18uksrgfwzM8LZFY",
   authDomain: "darwin-portal.firebaseapp.com",
@@ -11,5 +13,11 @@ const firebaseConfig = {
   measurementId: "G-R7LQ8MLYH69",
 };
 
+// 初始化
 export const app = initializeApp(firebaseConfig);
+
+// Firestore（必需）
 export const db = getFirestore(app);
+
+// Auth（后台登录必需）🔥🔥🔥
+export const auth = getAuth(app);
